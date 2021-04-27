ba9a7baa57a1b3047cd8eee2e9614bf69892e780
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-04-27 10:38:55 -0400

    h

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-
+import { UserProvider } from './context/userContext'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+     </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
