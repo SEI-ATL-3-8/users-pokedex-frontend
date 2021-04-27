da3c7e37f0df76b33e27253ee2d692fa119fce49
Jason Ouyang <jasonouyang8000@gmail.com>
2021-04-26 18:04:50 -0700

    login functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,46 @@
 import Form from "../components/Form"
 
+import {useContext} from 'react';
+import { UserContext } from "../context/userContext";
+
+
 
 const Login = () => {
+    const [userToken, setUserToken] =  useContext(UserContext).userTokenState;
+
+
+    const handleSubmit = async(e, formParams) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:3001/user/login', {
+                method: 'POST',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formParams)
+            });
+    
+            const data = await response.json();
+    
+            console.log(data);
+    
+            localStorage.setItem('userToken', data.userToken);
+    
+            setUserToken(data.userToken);
+        }
+
+        catch(error) {
+            console.log(error);
+        }
+       
+    }
 
     
     return (
         <div className="login-section">
             <h1>Login</h1>
-            <Form buttonName="Login" />
+            <Form handleSubmit={handleSubmit} buttonName="Login" />
 
         </div>
     )
