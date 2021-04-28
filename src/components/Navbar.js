89637007076ca12eef189bf607d6dae46933a87c
Ismail <ismail.desouki@gmail.com>
2021-04-28 08:04:41 -0700

    hw

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,54 @@
-import {Link} from 'react-router-dom'
+import { useReducer } from 'react';
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const testVar = true
     return (
         <ul>
-            <li>
-                <Link 
-                to="/">
-                    All Pokemon
-                </Link>
-            </li>
-            <li>
-                <Link 
-                to="/favorites">
-                    My Favorites
-                </Link>
-            </li>
+            {/* { testVar ? 'yes' : 'no' } */}
+            {/* { testVar ? 'yes' : null } */}
+            {/* { testVar && 'yes' } */}
+
+            { props.user.id ? <>
+                <li>
+                    <Link
+                    to="/">
+                        All Pokemon
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                    to="/favorites">
+                        My Favorites
+                    </Link>
+                </li>
+                <li>
+                    <span
+                        onClick={() => {
+                            localStorage.removeItem('userId')
+                            props.setUser({})
+                        }}
+                    >
+                        Log Out
+                    </span>
+                </li>
+            </>
+            :
+            <>
+                <li>
+                    <Link
+                    to="/login">
+                        Log In
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                    to="/signup">
+                        Sign Up
+                    </Link>
+                </li>
+            </>}
+
         </ul>
     )
 }
